158dc6b30753f0843133f288ad74a478d73469ca
Kieran Martin <kieranjmartin@gmail.com>
2024-07-08 09:12:26 +0100

    correct testing

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -1,11 +1,10 @@
 test_that("hello() works", {
   expect_output(hello(), "Hello, World!")
+  expect_equal(hello(), "Hello, World!")
 })
+
+
 
 test_that("hello_cli() works", {
   expect_message(hello_cli(), "Hello, World!")
-})
-
-test_that("fix me!!!", {
-  expect_equal(1, 2)
 })
